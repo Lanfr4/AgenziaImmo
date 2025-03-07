aa5698962155bfb5b627e020292879dc3bc95ffe
Lanfr4_ <sebalanfro@gmail.com>
2025-03-07 17:17:35 +0100

    updates

--- php/abitazioni.php
+++ php/abitazioni.php
@@ -6,7 +6,7 @@
     writeMenu();
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         // Recupera i dati dal modulo
-        $tipoCasa = $_POST['tipodiCasa'];
+        $tipoCasa = $_POST['tipoCasa'];
         $quartiere = $_POST['quartiere'];
     
         // Stampa i dati
@@ -16,8 +16,8 @@
     echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
 
 
-    echo "Tipo di Casa: " . htmlspecialchars($tipoCasa) . "<br>";
-    echo "Quartiere: " . htmlspecialchars($quartiere) . "<br>";
+    //echo "Tipo di Casa: " . htmlspecialchars($tipoCasa) . "<br>";
+    //echo "Quartiere: " . htmlspecialchars($quartiere) . "<br>";
 
     writefooter();
 
