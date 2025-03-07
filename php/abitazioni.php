21d9276d4e2f799a485648d3f07fadffa2f32451
Lanfr4_ <sebalanfro@gmail.com>
2025-03-07 16:14:57 +0100

    update

--- php/abitazioni.php
+++ php/abitazioni.php
@@ -5,23 +5,8 @@
     writeheader();
     writeMenu();
 
-
-    echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
-
-    $abitazione = $_REQUEST['tipodiCASA'];
-    $quartiere = $_REQUEST['quartiere'];
-
-    echo $abitazione;
-    echo $quartiere;
-
-
-
-
-
-
-
-
-
+    //echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
 
     writefooter();
+
 ?>
