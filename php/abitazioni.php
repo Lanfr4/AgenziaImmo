93b52d8f68c1fb84d952e9dd522b6ba905b3b5fe
Lanfr4_ <sebalanfro@gmail.com>
2025-03-07 17:15:56 +0100

    UPDATE IMPORTANTI

--- php/abitazioni.php
+++ php/abitazioni.php
@@ -4,8 +4,20 @@
 
     writeheader();
     writeMenu();
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Recupera i dati dal modulo
+        $tipoCasa = $_POST['tipodiCasa'];
+        $quartiere = $_POST['quartiere'];
+    
+        // Stampa i dati
+        
+    }
 
-    //echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
+    echo('<div class="alert alert-success">Pare funzionare tutto!</div>');
+
+
+    echo "Tipo di Casa: " . htmlspecialchars($tipoCasa) . "<br>";
+    echo "Quartiere: " . htmlspecialchars($quartiere) . "<br>";
 
     writefooter();
 
