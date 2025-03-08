d189ad3bec40ad13d716fb28a8d44f5131c07a36
Lanfr4_ <sebalanfro@gmail.com>
2025-03-08 11:53:40 +0100

    upload

--- php/abitazioni.php
+++ php/abitazioni.php
@@ -31,15 +31,15 @@
         // Iterazione attraverso i risultati
         while ($record = $resultSet->fetch_assoc()) {
             echo('<tr>
-                    <th scope="row">' .($record['id']) . '</th>
-                    <td>' . ($record['stato']) . '</td>
-                    <td>' . ($record['type']) . '</td>
-                    <td>' . ($record['prezzoRichiesto']) . '</td>
-                    <td>' . ($record['superfice']) . '</td>
-                    <td>' . ($record['quartiere']) . '</td>
-                    <td>' . ($record['indirizzo']) . '</td>
-                    <td>' . ($record['Data_disponibilita']) . '</td>
-                </tr>');
+                    <th scope="row">' .$record['id'] . '</th>
+                    <td>' . $record['stato'] . '</td>
+                    <td>' . $record['type'] . '</td>
+                    <td>' . $record['prezzoRichiesto']) . '</td>
+                    <td>' . $record['superfice'] . '</td>
+                    <td>' . $record['quartiere'] . '</td>
+                    <td>' . $record['indirizzo'] . '</td>
+                    <td>' . $record['Data_disponibilita'] . '</td>
+                </tr>';
         }
         echo('</tbody>
             </table>');
