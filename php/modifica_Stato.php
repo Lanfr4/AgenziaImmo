39a4a395b3d16bf1aed4bfe3764b430cb48370ea
Lanfr4_ <sebalanfro@gmail.com>
2025-03-10 19:33:28 +0100

    ui

--- php/modifica_Stato.php
+++ php/modifica_Stato.php
@@ -1,13 +1,23 @@
 <?php
 
-require('../include/lib.php');
+    require('../include/lib.php');
 
-// Connessione al database
-$DBHOST = "localhost";
-$DBUSER = "root";
-$DBPASSWORD = "root";
-$DBNAME = "csagenziaimmo";
+    // Connessione al database
+    $DBHOST = "localhost";
+    $DBUSER = "root";
+    $DBPASSWORD = "root";
+    $DBNAME = "csAgenziaImmo";
 
-$db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Recupera i dati dal modulo
+        $id = $_POST['id'];
+    }
+
+     //echo "Tipo Casa: " . htmlspecialchars($id) . "<br>";
+
+    $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
+    $sql = "UPDATE CS_IMMOBILE SET stato = 'Riservato' WHERE id = '$id'";
+    $result = $db->query($sql);
+    $db->close();
 
 ?>
