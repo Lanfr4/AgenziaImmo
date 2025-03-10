a04b7fc06d1ff76da8a9006fea327c202394c045
Lanfr4_ <sebalanfro@gmail.com>
2025-03-10 16:39:55 +0100

    updates

--- php/abitazioni.php
+++ php/abitazioni.php
@@ -30,15 +30,59 @@
                         <th scope="col">Quartiere</th>
                         <th scope="col">Indirizzo</th>
                         <th scope="col">Data Disponibilità</th>
+                        <th scope="col">Azione</th>
                     </tr>
                 </thead>
                 <tbody>');
 
-             
 
-            // Chiusura della connessione
+        
+        while ($record = $resultSet->fetch_assoc()) {
+            echo('<tr>
+                    <th scope="row">' . $record['id'] . '</th>
+                    <td>' . $record['stato'] . '</td>
+                    <td>' . $record['type'] . '</td>
+                    <td>' . $record['prezzoRichiesto'] . '</td>
+                    <td>' . $record['superfice'] . '</td>
+                    <td>' . $record['quartiere'] . '</td>
+                    <td>' . $record['indirizzo'] . '</td>
+                    <td>' . $record['Data_disponibilita'] . '</td>
+                    <td>
+                        <button type="button" onclick="yourFunction(' . $record['id'] . ')">Azione</button>
+                        <a href="modifiche.php?id=' . $record['id'] . '" class="btn btn-primary">Azione</a>
+                    </td>
+                </tr>');
+        }
+        echo('</tbody>
+            </table>');
+
+        // Chiusura della connessione
+        $db->close();
+    
+
+/*
+        else{// Il cliente ha scelto un determinato quartiere dove guardare un determinato tipo di edificio
+
+            while ($record = $resultSet->fetch_assoc()) {
+                if($record.['quartiere'] == $quartiere && $record.['tipocasa'] == $tipocasa){
+                echo('<tr>
+                        <th scope="row">' .$record['id'] . '</th>
+                        <td>' . $record['stato'] . '</td>
+                        <td>' . $record['type'] . '</td>
+                        <td>' . $record['prezzoRichiesto']) . '</td>
+                        <td>' . $record['superfice'] . '</td>
+                        <td>' . $record['quartiere'] . '</td>
+                        <td>' . $record['indirizzo'] . '</td>
+                        <td>' . $record['Data_disponibilita'] . '</td>
+                    </tr>';
+                }
+            echo('</tbody>
+                </table>');
+            }
             $db->close();
-            
+        }
+        
+*/
         writefooter();
 ?>
 
