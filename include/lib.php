1101c466ba70909620aa133676cd184581641f18
Sdrogo-sbr <kristian.sasso@galileiostiglia.edu.it>
2025-03-10 19:09:05 +0100

    sium

--- include/lib.php
+++ include/lib.php
@@ -5,7 +5,7 @@ il file sarà incluso nei file php che si trovano in /php. */
     $DBHOST= "localhost";
     $DBUSER= "root";
     $DBPASSWORD= "root";
-    $DBNAME ="csagenziaimmo";
+    $DBNAME ="csAgenziaImmo";
 
 /**
  * Funzione che genera la parte iniziale di ogni pagina, costruendo il corpo della pagina partendo dai tag fondamentali, genera il codice fino
