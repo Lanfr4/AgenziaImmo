de9bf2cb39d1e0be540ea4a0a19d326966b00f2b
Lanfr4_ <sebalanfro@gmail.com>
2025-03-11 22:38:08 +0100

    u

--- 2.0/admin/index.php
+++ 2.0/admin/index.php
@@ -5,30 +5,32 @@ require('../include/lib.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
-
+}
     $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
 
     // Query preparata per sicurezza
-    $stmt = $db->prepare("SELECT * FROM CS_ADMIN WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $resultSet = $stmt->get_result();
-    $db->close();
+    $sql = "SELECT a.email 
+            FROM CS_ADMIN AS a WHERE a.email = '$email'";
+    $resultSet = $db->query($sql);
 
     if ($resultSet->num_rows > 0) {
+        
         $record = $resultSet->fetch_assoc();
+        $sql1 = "SELECT a.password
+                FROM CS_ADMIN AS a WHERE a.password = '$password'";
+        $resultSet = $db->query($sql);
 
         // Verifica della password
-        if (password_verify($password, $record['password'])) {
+        if ($resultSet->num_rows > 0) {
             $_SESSION['logged'] = true;
             $_SESSION['idAdmin'] = $record['id'];
             $_SESSION['cognome'] = $record['cognome'];
             $_SESSION['nome'] = $record['nome'];
 
             // Login riuscito
-
             writeMenu();
-        } else {
+        } 
+        else {
             // Password errata
             header("Location: login.php"); // Reindirizza alla pagina di login
             exit;
@@ -38,5 +40,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header("Location: login.php"); // Reindirizza alla pagina di login
         exit;
     }
-}
 ?>
