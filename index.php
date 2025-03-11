43fda6ea9a6ccb6253971b00f51e3197ed66dbfe
Lanfr4_ <sebalanfro@gmail.com>
2025-03-11 14:29:48 +0100

    updates

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-
-    header('./2.0/index.php');
-
+/* questo file è il nostro punto di login per l' utente, 
+se decide di loggarsi passa alla zona amministratore solo per l' utente dell' agenzia,
+altrimenti passa direttamente alla zona pubblica*/
+    
+    header("location: ./2.0/admin/index.php");
 
 ?>
