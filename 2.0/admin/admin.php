91bce655e8a667ea64bd95f5ce7b8b0dcf8234c3
Lanfr4_ <sebalanfro@gmail.com>
2025-03-11 15:43:14 +0100

    u

--- 2.0/admin/admin.php
+++ 2.0/admin/admin.php
@@ -1,28 +1,13 @@
 <?php
-session_start();
-
-if (!isset($_SESSION['logged'])) {
-    $_SESSION['logged'] = false;
-}
-
-require("../include/lib.php");
-
-writeheader();
-
-if ($_SESSION['logged'] == true) {
-    writeMenu();
+    session_start();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Recupera i dati dal modulo
         $email = $_POST['email'];
         $password = $_POST['password'];
-
-        // Display the email and password (for demonstration purposes only)
-        echo "EMAIL: " . htmlspecialchars($email) . "<br>";
-        echo "PASSWORD: " . htmlspecialchars($password) . "<br>";
     }
-} else {
-    echo('<a href="login.php">Utente non loggato, eseguire il login</a>');
-}
-writefooter();
+
+
+    
+    echo('tutto apposto');
 ?>
