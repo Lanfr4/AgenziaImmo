97343ac88b054992c2185e6693aea1fdff36b73a
Lanfr4 <sebalanfro@gmail.com>
2025-03-13 09:39:29 +0100

    updates

--- 2.0/include/lib.php
+++ 2.0/include/lib.php
@@ -86,6 +86,22 @@ function writeMenu() {
                     </div>
             </div>
         </nav>
+        <br>
+        <div class="container-fluid row">
+                <div class="col-3">
+                    <form name="vendita" action = "modifiche.php>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                </div>
+                <div class="col-3">
+                    <form name="Aggiunbgi" action = "modifiche.php>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </form>
+        </div>
     ');
 
     return;
