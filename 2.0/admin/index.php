e51ef9ea70aee0096d1b7df13d7f4328353a76e2
Lanfr4 <sebalanfro@gmail.com>
2025-03-13 09:56:38 +0100

    u

--- 2.0/admin/index.php
+++ 2.0/admin/index.php
@@ -29,8 +29,8 @@
             }
             $_SESSION['logged'] = true;
             $_SESSION['idAdmin'] = 1;
-            $_SESSION['cognome'] = 'Agenzia';
-            $_SESSION['nome'] = 'Immobiliare';
+            $_SESSION['cognome'] = $record['cognome'];
+            $_SESSION['nome'] = $record['nome'];
             
             if($sc == "logout"){
                 $_SESSION['logged'] = false;
@@ -56,7 +56,6 @@
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-            <div id="emailHelp" class="form-text">We\'ll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
