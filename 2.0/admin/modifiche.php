8c4faaa89f7d16fbde1a7325337d23f6ba6b9dc5
Lanfr4_ <sebalanfro@gmail.com>
2025-03-18 16:58:49 +0100

    upd

--- 2.0/admin/modifiche.php
+++ 2.0/admin/modifiche.php
@@ -12,30 +12,19 @@
     if($_SESSION['logged'] == true){
         writeMenu();
             switch($sc){
-                case "venditaAbitazione" :{
-
-                    exit();
-                };
-
-                case "aggiungiAbitazione" :{
-                    echo('
-                        <form>
-                            <div class="mb-3">
-                                
-                            </div>
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Aggiungi</button>
-                        </form>');
-                    exit();
-                };
-
-                case "gestisciAbitazione" :{
+                case "venditaAbitazione" :{ // Bho
                     
-                    exit();
+                    break;
+                };
+
+                case "aggiungiAbitazione" :{// Sebastiano
+                    
+                    break;
+                };
+
+                case "gestisciAbitazione" :{ //Sasso
+                    
+                    break;
                 };
             }
         }
