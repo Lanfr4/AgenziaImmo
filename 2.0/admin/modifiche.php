f2359a0e3e054c531dcf0b3a460ce2ea3f06ea8d
Sdrogo-sbr <kristian.sasso@galileiostiglia.edu.it>
2025-03-19 20:03:20 +0100

    dio

--- 2.0/admin/modifiche.php
+++ 2.0/admin/modifiche.php
@@ -18,18 +18,7 @@
                 };
 
                 case "aggiungiAbitazione" :{
-                    echo('
-                        <form>
-                            <div class="mb-3">
-                                
-                            </div>
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Aggiungi</button>
-                        </form>');
+                    
                     exit();
                 };
 
