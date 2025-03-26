e74002c0db757a2bb4e77d4041d6bb3697774190
Lanfr4_ <sebalanfro@gmail.com>
2025-03-26 10:04:00 +0100

    updates

--- 2.0/admin/vendita.php
+++ 2.0/admin/vendita.php
@@ -21,25 +21,37 @@
 
         $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME); 
 
-       $sql ="SELECT * FROM CS_IMMOBILE AS imm, CS_ACQUIRENTE AS acq
-       WHERE $id = imm.id AND $idACQ = acq.id"; 
+       $sql ="SELECT *
+                FROM CS_IMMOBILE AS imm, CS_ACQUIRENTE AS acq
+                WHERE imm.prezzoRichiesto <= acq.capitale AND imm.id = $id AND acq.id = $idAcq"; 
        
-       echo($sql);
-
-       /*$resultSet = $db->query($sql);
+        $resultSet = $db->query($sql);
 
        if($resultSet->num_rows>=1){
-         
-       }
-       else{
 
-       }  
-        //echo($sql);
-    }*/
+        // si aggiorna il capitale dell' acquirente che ha comprato 
+        $sql2 = "UPDATE CS_ACQUIRENTE AS acq 
+                SET acq.capitale = acq.capitale - 
+                (
+                    SELECT imm.prezzoRichiesto 
+                    FROM CS_IMMOBILE AS imm 
+                    WHERE imm.id = $id
+                )
+                WHERE acq.id = $idAcq";
+            
+        $resultSet= $db->query($sql2);
+
+        // si va a inserire nella tabella vendita tutti i dati relativi alla vendita
+        $sql3 = "INSERT INTO CS_VENDITA ";
+
+        // si cambia stato oppure si toglia l' edificio dal catalogo, devo decicdere
+        $sql4 ="";
+        }
+        else{
+            echo('<a href="index.php">Vendita NON  conclusa </div>');
+        }  
     }
     else{
         echo('<a href="home.php">Utente non loggato, eseguire il login</div>');
     }
-
-
 ?>
