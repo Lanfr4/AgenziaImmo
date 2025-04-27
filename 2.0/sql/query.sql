cab889dc7c72774fffb7a99a4c0684b075e8e0a5
Lanfr4_ <sebalanfro@gmail.com>
2025-04-27 20:30:53 +0200

    UPD

--- 2.0/sql/query.sql
+++ 2.0/sql/query.sql
@@ -2,30 +2,12 @@
 ----------------------
 --1 Elenco degli immobili (con codice, indirizzo, prezzo) che soddisfano alle richieste di un acquirente, 
 --usando come criteri di ricerca: limite di prezzo, quartiere di collocazione, tipologia di immobili;
-<<<<<<< HEAD
 
 --2 Elenco degli immobili di un certo quartiere 
 --(con codice, indirizzo, tipologia di immobile e prezzo) che sono immediatamente disponibili e nello stato "offerto";
 
 --3 Elenco (con codice, indirizzo, tipologia di immobile e prezzo) 
 --degli immobili di un quartiere per i quali è in atto una trattativa, ordinato per tipologia di immobile e per prezzo;
-=======
-
-
-
---2lenco degli immobili di un certo quartiere 
---(con codice, indirizzo, tipologia di immobile e prezzo) che sono immediatamente disponibili e nello stato "offerto";
-SELECT codice, indirizzo, tipologia, prezzo
-FROM immobili
-WHERE quartiere = :quartiere AND stato = 'offerto';
-
---3 Elenco (con codice, indirizzo, tipologia di immobile e prezzo) 
---degli immobili di un quartiere per i quali è in atto una trattativa, ordinato per tipologia di immobile e per prezzo;
-SELECT codice, indirizzo, tipologia, prezzo
-FROM immobili
-WHERE quartiere = idQuartiere AND stato = 'in trattativa'
-ORDER BY tipologia, prezzo;
->>>>>>> a3c5d711ed26783b009ce61d8cd0b3209b8b93fc
 
 --4 Numero di telefono dell’acquirente o del proprietario di un dato immobile per organizzare una visita;
 
